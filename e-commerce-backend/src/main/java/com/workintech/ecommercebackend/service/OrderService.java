84a9f324a5cf3b2a3b2bfceab1712f4fbe6afb36
GayeBuse <gayebuseozkan@gmail.com>
2024-05-21 18:32:31 +0300

    Order  Service

---
+++ e-commerce-backend/src/main/java/com/workintech/ecommercebackend/service/OrderService.java
@@ -0,0 +1,12 @@
+package com.workintech.ecommercebackend.service;
+
+import com.workintech.ecommercebackend.entity.Order;
+
+import java.util.List;
+
+public interface OrderService {
+    List<Order> findAll();
+    Order findById(Long id);
+    Order save(Order order);
+    Order delete(Long id);
+}
---
+++ e-commerce-backend/src/main/java/com/workintech/ecommercebackend/service/OrderServiceImpl.java
@@ -0,0 +1,47 @@
+package com.workintech.ecommercebackend.service;
+
+import com.workintech.ecommercebackend.entity.Order;
+import com.workintech.ecommercebackend.repository.OrderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class OrderServiceImpl implements OrderService {
+    private OrderRepository orderRepository;
+
+    @Autowired
+    public OrderServiceImpl(OrderRepository orderRepository) {
+        this.orderRepository = orderRepository;
+    }
+
+    @Override
+    public List<Order> findAll() {
+        return orderRepository.findAll();
+    }
+
+    @Override
+    public Order findById(Long id) {
+        Optional<Order> optionalOrder = orderRepository.findById(id);
+        if (optionalOrder.isPresent()) {
+            return optionalOrder.get();
+        } return null; // hata yaz
+    }
+
+    @Override
+    public Order save(Order order) {
+        return orderRepository.save(order);
+    }
+
+    @Override
+    public Order delete(Long id) {
+        Order order = findById(id);
+        if (order != null) {
+            orderRepository.delete(order);
+            return order;
+        } return null;//hata yaz
+
+    }
+}
