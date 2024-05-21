5d90cf25c4da359749daf490e1c61fdcfdce32e1
GayeBuse <gayebuseozkan@gmail.com>
2024-05-21 17:41:32 +0300

    user entity

--- e-commerce-backend/src/main/java/com/workintech/ecommercebackend/entity/User.java
+++ e-commerce-backend/src/main/java/com/workintech/ecommercebackend/entity/User.java
@@ -1,4 +1,70 @@
 package com.workintech.ecommercebackend.entity;
 
-public class User {
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import java.util.*;
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+@Table(name = "user",schema = "e-commerce")
+
+public class User implements UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+    @NotBlank(message = "It is mandatory to fill in the name field")//anotasyonu, bir alanın null olmamasını, boş bir string ("") olmamasını ve yalnızca beyaz boşluk karakterlerinden (" ") oluşmamasını sağlar.
+    @Size(min = 3,max = 35,message = "You must enter at least 3 characters and greater than 35 characters.")
+    @Column(name = "name")
+    private String name;
+
+    @NotBlank(message = "Must be a valid email address.")
+    @Size(min = 8,max = 100,message = "Password must not be less than 8 and greater than 100 characters.")
+    @Column(name = "email")
+    private String email;
+    @NotBlank(message = "Password must be at least 8 characters long.")
+    @Size(min = 8,max = 100,message = "Password must not be less than 8 and greater than 100 characters.")
+    @Column(name = "password")
+    private String password;
+    @ManyToMany(fetch = FetchType.EAGER) // Roller, kullanıcı sorgulandığında hemen getirilir. Bu, rollerin veri tabanından hemen yüklenmesini sağlar.
+    @JoinTable(name = "user_role", schema = "ecommerce",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private List<Role> authorities = new ArrayList<>(); // Bu, kullanıcının rollerinin saklanacağı bir liste tanımlar.
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities; //Bu liste, Spring Security'nin kullanıcının yetkilerini anlaması için gereklidir.
+    }
+
+    @Override
+    public String getUsername() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
 }
