8c13dcf0b3144bcdbda222b8b56afa8b26c5a9d0
GayeBuse <gayebuseozkan@gmail.com>
2024-05-21 17:42:15 +0300

    Order entity

---
+++ e-commerce-backend/src/main/java/com/workintech/ecommercebackend/entity/Order.java
@@ -0,0 +1,44 @@
+package com.workintech.ecommercebackend.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.List;
+
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name = "order",schema = "ecommerce")
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "order_date")
+    private LocalDate orderDate;
+
+    @Column(name = "price")
+    private Double price;
+
+    @ManyToOne(cascade = {CascadeType.MERGE,CascadeType.PERSIST,CascadeType.DETACH,CascadeType.REFRESH})
+    @JoinColumn(name = "address_id")
+    private Address address;
+//birçok sipariş (Order) bir adres (Address) ile ilişkili olabilir.
+    @ManyToOne(cascade = {CascadeType.MERGE,CascadeType.PERSIST,CascadeType.DETACH,CascadeType.REFRESH})
+    @JoinColumn(name = "card_id")
+    private Card card;
+// Birçok Order bir Card ile ilişkilidir. Bu, bir kartın birden fazla sipariş için kullanılabileceği anlamına gelir
+
+    @ManyToMany(cascade = {CascadeType.MERGE,CascadeType.PERSIST,CascadeType.DETACH,CascadeType.REFRESH})
+    @JoinTable(name = "product_order",schema = "ecommerce",
+            joinColumns = @JoinColumn(name = "order_id"),
+            inverseJoinColumns = @JoinColumn(name = "product_id"))
+    private List<Product> productList;
+    //Bir siparişin birden fazla ürün içerebileceğini belirtir. Bu ilişki "product_order" adlı ilişki tablosu üzerinden gerçekleştirilir.
+}
