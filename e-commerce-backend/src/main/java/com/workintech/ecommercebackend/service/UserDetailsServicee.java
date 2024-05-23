da4cf652bc2203589d5f2900d983a080b5f7eddd
GayeBuse <gayebuseozkan@gmail.com>
2024-05-23 17:32:22 +0300

    User Service

--- e-commerce-backend/src/main/java/com/workintech/ecommercebackend/service/UserDetailsServicee.java
+++ e-commerce-backend/src/main/java/com/workintech/ecommercebackend/service/UserDetailsServicee.java
@@ -5,7 +5,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-
+//UserDetailsServicee sınıfı,
+// Spring Security'nin bir parçasıdır
+// ve kullanıcı kimlik doğrulama
+// işlemlerini gerçekleştirir.
+// Bu sınıf, kullanıcı bilgilerini
+// yüklemek için bir servis sağlar ve
+// özellikle kullanıcı adı (veya e-posta)
+// temelinde kullanıcıları bulur.
+//Bu sınıf olmadan, Spring Security
+// ile kullanıcı kimlik doğrulaması
+// yapılamaz. Kimlik doğrulama işlemleri,
+// kullanıcı bilgilerini yükleyemez ve
+// güvenlik kontrolleri çalışmaz.
 public class UserDetailsServicee implements UserDetailsService {
     private UserRepository userRepository;
 @Autowired
--- e-commerce-backend/src/main/java/com/workintech/ecommercebackend/service/UserService.java
+++ e-commerce-backend/src/main/java/com/workintech/ecommercebackend/service/UserService.java
@@ -3,7 +3,7 @@ package com.workintech.ecommercebackend.service;
 import com.workintech.ecommercebackend.entity.User;
 
 import java.util.List;
-
+//UserService: Kullanıcı kayıtlarıyla ilgili temel veritabanı işlemlerini içerir. Kullanıcı bilgilerini getirir, yeni kullanıcı oluşturur, mevcut kullanıcıyı günceller veya siler.
 public interface UserService {
     User findById(Long id);
     List<User> findAll();
