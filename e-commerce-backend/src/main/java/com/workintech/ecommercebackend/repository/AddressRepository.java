75ef6e7ad73838d79a9d19ce1498c2958a8d2da6
GayeBuse <gayebuseozkan@gmail.com>
2024-05-27 23:24:49 +0300

    Address Repository update

--- e-commerce-backend/src/main/java/com/workintech/ecommercebackend/repository/AddressRepository.java
+++ e-commerce-backend/src/main/java/com/workintech/ecommercebackend/repository/AddressRepository.java
@@ -2,7 +2,8 @@ package com.workintech.ecommercebackend.repository;
 
 import com.workintech.ecommercebackend.entity.Address;
 import org.springframework.data.jpa.repository.JpaRepository;
-// Bu, her bir entity sınıfının veritabanı işlemlerini yönetmek için bir repository sınıfına sahip olması anlamına gelir. Bu yaklaşım, veri erişim katmanının modüler ve yönetilebilir olmasını sağlar
+// Bu, her bir entity sınıfının veritabanı işlemlerini yönetmek için bir repository sınıfına sahip olması anlamına gelir.
+// Bu yaklaşım, veri erişim katmanının modüler ve yönetilebilir olmasını sağlar
 //JpaRepository arayüzünü genişlettiği için AddressRepository şu temel CRUD işlemlerini sağlar:
 //Create: Yeni bir Address entity'sini veri tabanına eklemek.
 //Read: Mevcut Address entity'lerini veri tabanından sorgulamak.
