04bcd97559058aa0b7afbaff09e2fee45dc592fb
GayeBuse <gayebuseozkan@gmail.com>
2024-05-27 23:25:42 +0300

    Address service update

--- e-commerce-backend/src/main/java/com/workintech/ecommercebackend/service/AddressService.java
+++ e-commerce-backend/src/main/java/com/workintech/ecommercebackend/service/AddressService.java
@@ -3,11 +3,12 @@ package com.workintech.ecommercebackend.service;
 import com.workintech.ecommercebackend.entity.Address;
 
 import java.util.List;
-
+//
 public interface AddressService {
-    List<Address>findAll();
+    List<Address>findAllAddresses();
     Address findById(long id);
     Address save(Address address);
     Address delete(long id);
+
 
 }
--- e-commerce-backend/src/main/java/com/workintech/ecommercebackend/service/AddressServiceImpl.java
+++ e-commerce-backend/src/main/java/com/workintech/ecommercebackend/service/AddressServiceImpl.java
@@ -1,6 +1,7 @@
 package com.workintech.ecommercebackend.service;
 
 import com.workintech.ecommercebackend.entity.Address;
+import com.workintech.ecommercebackend.exceptions.GlobalExceptions;
 import com.workintech.ecommercebackend.repository.AddressRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -8,20 +9,29 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
-
+//Bu kod parçacığı, bir Spring Boot
+// uygulamasında adres işlemlerini
+// gerçekleştirmek için kullanılan bir
+// hizmet katmanını (service layer) temsil
+// ediyor. İki ayrı sınıf var çünkü tipik bir tasarım
+// deseni olan "Dependency Injection" (Bağımlılık Enjeksiyonu) kullanıldı.
+//Bu yapı, kodun modülerliğini ve bakımını artırır. Bağımlılık Enjeksiyonu
+// sayesinde, AddressServiceImpl sınıfı AddressRepository'ye ihtiyaç duyar ve
+// bu bağımlılık Spring tarafından otomatik olarak enjekte edilir. Bu, sınıflar
+// arasındaki sıkı bağımlılığı azaltır ve değişikliklerin kolayca yapılmasını sağlar.
+// Ayrıca, bu yapı test edilebilirlik açısından da avantaj sağlar.
 @Service
 public class AddressServiceImpl implements AddressService {
-
-
     private AddressRepository addressRepository;
     @Autowired
     public AddressServiceImpl(AddressRepository addressRepository) {
         this.addressRepository = addressRepository;
     }
     @Override
-    public List<Address> findAll() {
+    public List<Address> findAllAddresses() {
         return addressRepository.findAll();
     }
+
 
     @Override
     public Address findById(long id) {//Bu metod, belirtilen id'ye sahip Address kaydını bulmaya çalışır ve sonucu bir Optional<Address> içinde döner.
@@ -31,7 +41,7 @@ public class AddressServiceImpl implements AddressService {
             return optionalAddress.get();
         }
 
-        return null; //hata fırlatılacak
+        throw new GlobalExceptions("Address is not found with id: " + id, HttpStatus.NOT_FOUND);
     }
     @Override
     public Address save(Address address) {
@@ -45,6 +55,6 @@ public class AddressServiceImpl implements AddressService {
             addressRepository.delete(address);
             return address;
         }
-       return null;//hata fırlatılacak
+        throw new GlobalExceptions("Address is not found with id: " + id, HttpStatus.NOT_FOUND);
     }
 }
