07e44feaa15394b03db75d115dc06a42906457f7
GayeBuse <gayebuseozkan@gmail.com>
2024-05-27 22:54:34 +0300

    exceptions

---
+++ e-commerce-backend/src/main/java/com/workintech/ecommercebackend/exceptions/ErrorResponse.java
@@ -0,0 +1,18 @@
+package com.workintech.ecommercebackend.exceptions;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+//Error response oluşturmak, kullanıcıya daha anlamlı ve
+// yapılandırılmış hata mesajları göndermek için önemlidir.
+// Bu mesajlar, hata türünü, açıklamasını ve gerektiğinde hatanın
+// nerede olduğunu belirtebilir.
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ErrorResponse {
+    private int status;
+    private String message;
+    private long timeStamp;
+}
---
+++ e-commerce-backend/src/main/java/com/workintech/ecommercebackend/exceptions/GlobalExceptions.java
@@ -0,0 +1,17 @@
+package com.workintech.ecommercebackend.exceptions;
+
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@Setter
+public class GlobalExceptions extends RuntimeException {
+    private HttpStatus httpStatus;
+
+    public GlobalExceptions(String message, HttpStatus httpStatus) {
+        super(message);
+        this.httpStatus = httpStatus;
+    }
+}
